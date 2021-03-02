cbc211bc3249f935c7cfa0796157e49ec945fbbd
Debapriya Das <Debapriya.Das@ril.com>
2021-03-02 16:10:25 +0530

    smol changes

--- index.js
+++ index.js
@@ -89,3 +89,26 @@ exports.handler = async (event) => {
     }
 
 }; 
+
+
+
+'use strict';
+
+module.exports.hello = async function(event) {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(
+      {
+        message: 'Hello World!',
+        params: event.queryStringParameters,
+        secret: process.env.A_VARIABLE
+      },
+      null,
+      2
+    ),
+  };
+};
+

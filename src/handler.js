0047919554566395872e24d689551fc1d512ebfa
Debapriya Das <Debapriya.Das@ril.com>
2021-03-02 15:44:47 +0530

    trying to fix this

--- src/handler.js
+++ src/handler.js
@@ -1,11 +1,16 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.hello = async function(event) {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify(
       {
-        message: 'Your function executed successfully!'
+        message: 'Hello World!',
+        params: event.queryStringParameters,
+        secret: process.env.A_VARIABLE
       },
       null,
       2

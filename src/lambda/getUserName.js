9b27408584eb80581648b277e8f50aa86cba5378
Aaron Tot <company-register@utopiatech.io>
2021-03-13 21:48:00 +1000

    body parser added

--- src/lambda/getUserName.js
+++ src/lambda/getUserName.js
@@ -3,12 +3,15 @@ let empolyees = [
     { "5ergvfd7580": { first_name: "Sara", last_name: "Saad", email: "sara128@gmail.com", phone: "54878621075", salary: 17000 } },
     { "123sefvd450": { first_name: "Mohammad", last_name: "Wael", email: "moh170@gmail.com", phone: "48786305437", salary: 20000 } },
 ]
+const parser = require("body-parser-for-serverless");
 
 
 exports.handler = async function (event) {
-
+event = await parser(event);
+ 
+console.log(event);
     //if no Token
-    if (!event.body) {
+    if (!event) {
         return {
             statusCode: 400,
             headers: { 'Access-Control-Allow-Origin': '*' },
@@ -16,9 +19,13 @@ exports.handler = async function (event) {
         }
     }
 
-    let { token } = JSON.parse(event.body);
+    let { token } = event;
 
     for (let empolyee in empolyees) {
+        console.log("Received ",token )
+        console.log("User token ",empolyee )
+        console.log("empolyee[token]",empolyee[token] )
+
         if (empolyee[token]) {
             return {
                 statusCode: 200,

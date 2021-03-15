506f2b2a2f7bd3dfc6e815cd78f2aca7ac27baaf
Aaron Tot <company-register@utopiatech.io>
2021-03-16 04:13:59 +1000

    user create completed

--- src/middleware/user/user_create.js
+++ src/middleware/user/user_create.js
@@ -2,17 +2,18 @@ const parser = require("body-parser-for-serverless");
 const handler = require('../handler')
 
 exports.handler = async (event, context) => {
+    var datetime = new Date().toISOString().slice(0, 19).replace('T', ' ');
     const body = await parser(event);
     var api_name = 'User create'
     const data = {
-        user_first_name: "aa",
-        user_middle_name: "aa",
-        user_last_name: "aa",
-        user_dob: "aa",
-        user_gender: "aa",
-        user_email: "aa",
-        user_password: "aa",
-        user_datetime_created: "aa",
+        user_first_name: body.user_first_name,
+        user_middle_name: body.user_middle_name,
+        user_last_name: body.user_last_name,
+        user_dob: body.user_dob,
+        user_gender: body.user_gender,
+        user_email: body.user_email,
+        user_password:body.user_password,
+        user_datetime_created: datetime,
         id_user_status: 1,
         id_user_access_level: 0,
         id_user_title: 1,

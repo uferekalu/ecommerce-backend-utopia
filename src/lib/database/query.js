c004e96819ba72232941b7e8d37584b04ef0d617
Madhu <madhu.java.git@gmail.com>
2021-04-07 11:50:55 +1000

    first commit

--- src/lib/database/query.js
+++ src/lib/database/query.js
@@ -1,24 +1,32 @@
 const mysql = require("mysql");
-const connection = require('./connect')
-const handler = require('../../middleware/handler')
+const connection = require("./connect");
+const handler = require("../../middleware/handler");
 
 module.exports = {
+  search_one: async (table, column, data) => {
+    let result = await connection.query(
+      `SELECT * FROM ${table} WHERE ${column} = ?`,
+      [data]
+    );
+    return result;
+  },
+  search_two: async (table, column_one, column_two, data1, data2) => {
+    let result = await connection.query(
+      `SELECT * FROM ${table} WHERE ${column_one} = ? AND ${column_two} = ?`,
+      [data1, data2]
+    );
+    return result;
+  },
+  insert_new: async (data, table) => {
+    const result = await connection.query(`INSERT INTO ${table} SET ?`, data);
+    return result;
+  },
 
-    search_one: async (table, column, data) => {
-        let result = await connection.query(`SELECT * FROM ${table} WHERE ${column} = ?`, [data])
-        return result
-
-    },
-
-    insert_new: async (data, table) => {
-        const result = await connection.query(`INSERT INTO ${table} SET ?`, data)
-        return result
-    },
-
-    update_one: async (table, updated_data, column, condition) => {
-        const result = await connection.query(`UPDATE ${table} SET ? WHERE ${column} = ?`, [updated_data, condition])
-        return result
-    }
-
-
-}
+  update_one: async (table, updated_data, column, condition) => {
+    const result = await connection.query(
+      `UPDATE ${table} SET ? WHERE ${column} = ?`,
+      [updated_data, condition]
+    );
+    return result;
+  },
+};

009bfb184fd56e65ea004166cdb500f3763afb6e
mejabidurotimi <mejabidurotimi@yahoo.com>
2021-04-29 11:09:03 +0100

    Added the id_user_access_level column to the users table

--- src/middleware/verify_token.js
+++ src/middleware/verify_token.js
@@ -1,9 +1,9 @@
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
 
-module.exports =  {
-    create_token : (id)=>{
-        const token = jwt.sign({ _id_user: id}, 'dev_key')// process.env.JWT_KEY);
-        return token;
-    }
-}
+module.exports = {
+  create_token: (id) => {
+    const token = jwt.sign({ _id_user: id }, "dev_key"); // process.env.JWT_KEY);
+    return token;
+  },
+};

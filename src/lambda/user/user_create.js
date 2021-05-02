ad528c8515704dc7efde96d37065a9df61301fcc
mejabidurotimi <mejabidurotimi@yahoo.com>
2021-05-02 16:18:30 +0100

    added token creation upon user signup

--- src/lambda/user/user_create.js
+++ src/lambda/user/user_create.js
@@ -1,32 +1,33 @@
-const handler = require("../../middleware/handler")
-const db = require("../../lib/database/query")
-const send = require("../../lib/services/email/send_email")
-const bcrypt = require("bcryptjs")
-const Cryptr = require("cryptr")
-const cryptr = new Cryptr("myTotalySecretKey")
+const handler = require("../../middleware/handler");
+const auth_token = require("../../middleware/verify_token");
+const db = require("../../lib/database/query");
+const send = require("../../lib/services/email/send_email");
+const bcrypt = require("bcryptjs");
+const Cryptr = require("cryptr");
+const cryptr = new Cryptr("myTotalySecretKey");
 
-const passwordHash = async password => {
-  const salt = await bcrypt.genSalt(10)
-  const passwordHash = await bcrypt.hash(password, salt)
-  return passwordHash
-}
+const passwordHash = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+  return passwordHash;
+};
 
-const api_name = "User create"
+const api_name = "User create";
 const email_info = {
   subject: "Email Verification",
   message: "Please click here to verify your email\n\n\n\n",
-} // we can send  HTML template insted of messgae
+}; // we can send  HTML template insted of messgae
 
 exports.handler = async (event, context) => {
   try {
-    var datetime = await handler.datetime()
-    const body = JSON.parse(event.body)
+    var datetime = await handler.datetime();
+    const body = JSON.parse(event.body);
 
     if (!body || JSON.stringify(body) === "{}") {
-      throw "body is empty"
+      throw "body is empty";
     }
 
-    const all_fields = Object.keys(body)
+    const all_fields = Object.keys(body);
 
     const required_fields = [
       "user_email",
@@ -34,12 +35,14 @@ exports.handler = async (event, context) => {
       "user_last_name",
       "user_password",
       "id_user_title",
-    ]
+    ];
 
-    const missing_fields = required_fields.filter(field => !all_fields.includes(field))
+    const missing_fields = required_fields.filter(
+      (field) => !all_fields.includes(field)
+    );
 
     if (missing_fields.length > 0) {
-      throw Error(missing_fields)
+      throw Error(missing_fields);
     }
 
     const {
@@ -49,15 +52,15 @@ exports.handler = async (event, context) => {
       user_password,
       id_user_title,
       ...others
-    } = body
+    } = body;
 
-    const email_exist = await db.search_one("users", "user_email", user_email)
+    const email_exist = await db.search_one("users", "user_email", user_email);
 
     if (email_exist.length != 0) {
-      throw "Email already exist"
+      throw "Email already exist";
     }
 
-    const password_hashed = await passwordHash(user_password)
+    const password_hashed = await passwordHash(user_password);
 
     const data = {
       user_first_name,
@@ -70,31 +73,35 @@ exports.handler = async (event, context) => {
       id_user_title,
       email_verified: 0,
       ...others,
-    }
+    };
 
-    const result = await db.insert_new(data, "users")
+    const result = await db.insert_new(data, "users");
 
-    const id_hashed = cryptr.encrypt(result.insertId)
-    email_info.message += `https://4l0nq44u0k.execute-api.us-east-2.amazonaws.com/staging/api/user_email_verify/${id_hashed}`
+    const id_hashed = cryptr.encrypt(result.insertId);
+    email_info.message += `https://4l0nq44u0k.execute-api.us-east-2.amazonaws.com/staging/api/user_email_verify/${id_hashed}`;
 
-    await send.send_email(body.user_email, email_info)
+    await send.send_email(body.user_email, email_info);
 
-    return handler.returner(
-      [true, { message: "Created account successful", id_user: result.insertId }],
-      api_name,
-      201
-    )
+    // console.log(auth_token.create_token(id_user));
+
+    let response = {
+      message: "Created account successful",
+      id_user: result.insertId,
+      token: auth_token.create_token(result.insertId),
+    };
+
+    return handler.returner([true, response], api_name, 201);
   } catch (e) {
     if (e.name === "Error") {
       const errors = e.message
         .split(",")
-        .map(field => {
-          return `${field} is required`
+        .map((field) => {
+          return `${field} is required`;
         })
-        .join(", ")
+        .join(", ");
 
-      return handler.returner([false, errors], api_name, 400)
+      return handler.returner([false, errors], api_name, 400);
     }
-    return handler.returner([false, e], api_name, 400)
+    return handler.returner([false, e], api_name, 400);
   }
-}
+};

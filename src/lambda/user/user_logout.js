8a8eff831cbd915643eca266120d2e7b7f50e9a4
Creativogee <omowole.gbenga@gmail.com>
2021-06-02 18:00:03 +0100

    user logout bug fix

--- src/lambda/user/user_logout.js
+++ src/lambda/user/user_logout.js
@@ -7,21 +7,21 @@ const { delete_one } = require("../../lib/database/query")
 //
 const api_name = "User logout"
 exports.handler = async (event, context) => {
-  try {
-    const body = JSON.parse(event.body)
+    try {
+        const body = JSON.parse(event.body)
 
-    const token_exist = await db.search_one("user_tokens", "token", body.token)
-    //*******if the token doesn't exist sendback false, { message: "User is not found" }
-    //*******if exists delete it from the user_tokens table send a success bool back
+        const token_exist = await db.search_one("user_tokens", "token", body.token)
+        //*******if the token doesn't exist sendback false, { message: "User is not found" }
+        //*******if exists delete it from the user_tokens table send a success bool back
 
-    if (token_exist.length == 0)
-      return handler.returner([false, { message: "User is not found" }], api_name, 404)
-    //if (token_exist) {
-    const deletedToken = await delete_one("user_tokens", "token", body.token)
-    return handler.returner([true], api_name, 201)
-    //}
-  } catch (e) {
-    console.log("Error: ", e)
-    return handler.returner([false, e], api_name)
-  }
+        if (token_exist.length > 0) {
+            await delete_one("user_tokens", "token", body.token)
+        }
+
+        return handler.returner([true], api_name, 201)
+        //}
+    } catch (e) {
+        console.log("Error: ", e)
+        return handler.returner([false, e], api_name)
+    }
 }

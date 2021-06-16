981d1e52b68a9cdc16f47bb29a6fe7f38e1ab2fb
Creativogee <omowole.gbenga@gmail.com>
2021-06-16 18:48:49 +0100

    reverting

--- src/lambda/vendor/vendor_products_get.js
+++ src/lambda/vendor/vendor_products_get.js
@@ -36,7 +36,7 @@ exports.handler = async (event, context) => {
         return handler.returner([true, data], api_name, 200)
     } catch (e) {
         if (e === error_one || e === error_two) {
-            return handler.returner([false, { message: e, data }], api_name, 404)
+            return handler.returner([false, { message: e, products: data }], api_name, 404)
         }
         return handler.returner([false, e], api_name, 500)
     }

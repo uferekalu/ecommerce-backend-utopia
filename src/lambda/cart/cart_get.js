aa42eda057ce7208eb878bd933826776e94edb39
Creativogee <omowole.gbenga@gmail.com>
2021-06-17 18:01:18 +0100

    added cart endpoints

---
+++ src/lambda/cart/cart_get.js
@@ -0,0 +1,75 @@
+const handler = require("../../middleware/handler")
+const db = require("../../lib/database/query")
+const auth_token = require("../../middleware/token_handler")
+
+const api_name = "Cart get"
+const errors_array = [
+    "body is empty",
+    "user does not exist",
+    "authentication required",
+    "your cart is empty",
+    "cart get unsuccessful",
+]
+
+exports.handler = async (event) => {
+    try {
+        const body = JSON.parse(event.body)
+
+        if (!body || JSON.stringify(body) === "{}") {
+            throw `${errors_array[0]}`
+        }
+
+        const all_fields = Object.keys(body)
+
+        const required_fields = ["token", "id_user"]
+
+        const missing_fields = required_fields.filter((field) => !all_fields.includes(field))
+
+        if (missing_fields.length > 0) {
+            throw Error(missing_fields)
+        }
+
+        const { id_user, token } = body
+
+        const user_exist = (await db.search_one("users", "id_user", id_user))[0]
+
+        if (!user_exist) {
+            throw `${errors_array[1]}`
+        }
+
+        const isAuthUser = await auth_token.verify(token)
+
+        if (id_user != isAuthUser) {
+            throw `${errors_array[2]}`
+        }
+
+        const cart = (await db.select_all_with_condition("carts", { id_user }))[0]
+
+        if (!cart) {
+            throw `${errors_array[3]}`
+        }
+
+        const data = JSON.parse(cart.cart_items)
+
+        return handler.returner([true, data], api_name, 200)
+    } catch (e) {
+        let errors
+        if (e.name === "Error") {
+            errors = e.message
+                .split(",")
+                .map((field) => {
+                    return `${field} is required`
+                })
+                .join(", ")
+        }
+
+        if (errors_array.includes(e)) {
+            errors = e
+        }
+
+        if (errors) {
+            return handler.returner([false, errors], api_name, 400)
+        }
+        return handler.returner([false, e], api_name, 500)
+    }
+}

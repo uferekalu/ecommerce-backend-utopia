59e6895bc0b2cc47e4f7d8ed3aa78da0b97589fe
Creativogee <omowole.gbenga@gmail.com>
2021-06-25 10:46:15 +0100

    connect to new db

--- src/lib/database/connect.js
+++ src/lib/database/connect.js
@@ -1,30 +1,14 @@
+"use strict"
 require("dotenv").config()
-;("use strict")
 
-let connection
-
-if (process.env.PRODUCTION) {
-    connection = require("serverless-mysql")({
-        config: {
-            database: process.env.DB_NAME,
-            user: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-        },
-    })
-}
-
-if (!process.env.PRODUCTION) {
-    connection = require("serverless-mysql")({
-        config: {
-            database: "utopia",
-            user: "admin",
-            password: "TY8jGx6x23",
-            host: "utopia-db-dev.cmj8spxpnzjj.us-east-2.rds.amazonaws.com",
-            port: "3306",
-        },
-    })
-}
+const connection = require("serverless-mysql")({
+    config: {
+        database: process.env.DB_NAME,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+    },
+})
 
 module.exports = connection

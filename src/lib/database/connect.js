9b47dd64d4ee2beeadfeaaa66d9cd46cc9638a4a
Creativogee <omowole.gbenga@gmail.com>
2021-06-25 06:50:03 +0100

    new db

--- src/lib/database/connect.js
+++ src/lib/database/connect.js
@@ -1,18 +1,9 @@
-"use strict"
+require("dotenv").config()
+;("use strict")
 
-/*
-console.log({//TODO: Need to setup a dev mysql db
-  config: {
-    database: process.env.DB_NAME,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  },
-})*/
-var connection
-//this will check if live on producion server. And change the db connection accordingly
-if (Object.keys(process.env).includes("AWS_LAMBDA_FUNCTION_VERSION 111111111")) {
+let connection
+
+if (process.env.PRODUCTION) {
     connection = require("serverless-mysql")({
         config: {
             database: process.env.DB_NAME,
@@ -22,9 +13,10 @@ if (Object.keys(process.env).includes("AWS_LAMBDA_FUNCTION_VERSION 111111111")) 
             port: process.env.DB_PORT,
         },
     })
-} else {
+}
+
+if (!process.env.PRODUCTION) {
     connection = require("serverless-mysql")({
-        //TODO: Need to setup a dev mysql db
         config: {
             database: "utopia",
             user: "admin",

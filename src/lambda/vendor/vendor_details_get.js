0e6220baf201cc83a49d88e6af53404c6487dff2
mejabidurotimi <mejabidurotimi@yahoo.com>
2021-07-05 11:17:10 +0100

    s

--- src/lambda/vendor/vendor_details_get.js
+++ src/lambda/vendor/vendor_details_get.js
@@ -1,12 +1,19 @@
 const handler = require("../../middleware/handler")
 const db = require("../../lib/database/query")
+const auth_token = require("../../middleware/token_handler")
 
 const api_name = "Vendor details"
 const error_one = "vendor not found"
 
 exports.handler = async (event, context) => {
     try {
-        const id_vendor = event.pathParameters.id_vendor
+        const body = JSON.parse(event.body)
+
+        const { token } = body
+
+        const id_user = await auth_token.verify(token)
+
+        const { id_vendor } = (await db.select_all_with_condition("users", { id_user }))[0]
 
         const vendor = await db.search_one("vendors", "id_vendor", id_vendor)
 

246f71c917227177eba29ad3152629bccbb41cad
Aaron Tot <company-register@utopiatech.io>
2021-07-08 01:25:33 +1000

    buyer product get vendor details

---
+++ src/lambda/vendor/vendor_public_details.js
@@ -0,0 +1,28 @@
+const handler = require("../../middleware/handler")
+const db = require("../../lib/database/query")
+
+const api_name = "Vendors"
+
+exports.handler = async (event, context) => {
+    try {
+        const { id_vendor } = event.pathParameters
+
+        console.log("bbbbbbbbbbbbbbbbbbbbbbbbb: ", id_vendor)
+
+        const columns = [
+            "id_vendor",
+            "business_name",
+            "vendor_long_desc",
+            "vendor_short_desc",
+            "vendor_photo"
+        ]
+
+
+        const vendor_public_details = await db.select_columns_with_condictions(columns, "vendors", "id_vendor", id_vendor)
+
+        return handler.returner([true,  vendor_public_details[0]], api_name, 200)
+    } catch (e) {
+        console.log("Error in ", api_name, " try catch: ", e)
+        return handler.returner([false], api_name, 500)
+    }
+}

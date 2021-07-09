94b91684518239a035bf1f232fab308af0dfc844
Ufere Kalu <dekalusha@gmail.com>
2021-07-09 13:30:33 -0600

    completed and cancelled order

---
+++ src/lambda/order/request_order_cancelled.js
@@ -0,0 +1,28 @@
+const handler = require("../../middleware/handler")
+const send = require("../../lib/services/email/send_email")
+
+const api_name = "Order completed";
+const email_info = {
+    subject: "Request Order cancelled",
+    message: "order id is ",
+} 
+// we can send  HTML template insted of messgae
+
+exports.handler = async (event, context) => {
+    try {
+        const body = JSON.parse(event.body)
+        const order_id = body.order_id
+
+        //Send email 
+        email_info.message += order_id;
+        await send.email('customercare@utopiatech.io', email_info);
+
+        return handler.returner(
+            [true, { order_id , message: "Your Order has been cancelled" }],
+            api_name,
+            200
+        )
+    } catch (e) {
+        return handler.returner([false, e], api_name)
+    }
+}
